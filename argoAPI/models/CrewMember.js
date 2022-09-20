8145485e9c98e9a9bce3fe39a21de32677e394ee
bdeliencourt <70108841+bdeliencourt@users.noreply.github.com>
2022-09-21 01:27:08 +0200

    Setup MongoDB CrewMember table

---
+++ argoAPI/models/CrewMember.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+// Define CrewMember table
+const CrewMember = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+);
+
+// Export model: Crew
+module.exports = mongoose.model('Crew', CrewMember);
