f51c6582e715a775b1a8a3b16775ea7fe83c32d1
bdeliencourt <70108841+bdeliencourt@users.noreply.github.com>
2022-09-21 03:57:06 +0200

    Update style

--- argoapp/src/pages/Home.js
+++ argoapp/src/pages/Home.js
@@ -10,11 +10,11 @@ const Home = () => {
       <div id="titleContainer" className="display-4 mb-5 mt-n3">
         <h2>
           Welcome aboard Jason 🌊🏛️ <br/>
-          <small class="text-muted lead">Manage your crew with the app! </small>
+          <small className="text-muted lead">Manage your crew with the app! </small>
         </h2>
       </div>
 
-      <Container className="crewContainer px-2 pt-2" fluid>
+      <Container className="crewContainer px-2 py-3" fluid>
         <CrewList />
       </Container>
         <FormCrew />
